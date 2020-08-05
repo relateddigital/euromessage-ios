a9428b4cf1d9edae3298eff318ac9806a57cdbcd
cicimen <egemengulkilik@gmail.com>
2020-08-05 15:54:23 +0300

    Example project files add

---
+++ Example/NotificationContent/EMNotificationViewController.swift
@@ -0,0 +1,43 @@
+//
+//  EMNotificationViewController.swift
+//  NotificationContent
+//
+//  Created by Muhammed ARAFA on 12.07.2020.
+//  Copyright © 2020 Muhammed ARAFA. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+import Euromsg
+
+@available(iOS 10.0, *)
+@objc(EMNotificationViewController)
+class EMNotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    let appUrl = URL(string: "euromsgExample://")
+    let carouselView = EMNotificationCarousel.initView()
+    var completion: ((_ url: URL?, _ userInfo: [AnyHashable: Any]?) -> Void)?
+    func didReceive(_ notification: UNNotification) {
+        carouselView.didReceive(notification)
+    }
+    func didReceive(_ response: UNNotificationResponse,
+                    completionHandler completion: @escaping (UNNotificationContentExtensionResponseOption) -> Void) {
+        carouselView.didReceive(response, completionHandler: completion)
+    }
+    override func loadView() {
+        completion = { [weak self] url, userInfo in
+            if let url = url {
+                self?.extensionContext?.open(url)
+                if url.scheme != self?.appUrl?.scheme, let userInfo = userInfo {
+                    Euromsg.handlePush(pushDictionary: userInfo)
+                }
+            }
+            else if let url = self?.appUrl {
+                self?.extensionContext?.open(url)
+            }
+        }
+        carouselView.completion = completion
+        self.view = carouselView
+    }
+}
