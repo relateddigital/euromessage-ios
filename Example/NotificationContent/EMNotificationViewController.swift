7e020acbd5a64e6a59b048cba11aed668df97d8f
cicimen <egemengulkilik@gmail.com>
2021-09-13 22:32:58 +0300

    example project @available(iOS 10.0, *) remove

--- Example/NotificationContent/EMNotificationViewController.swift
+++ Example/NotificationContent/EMNotificationViewController.swift
@@ -11,7 +11,6 @@ import UserNotifications
 import UserNotificationsUI
 import Euromsg
 
-@available(iOS 10.0, *)
 @objc(EMNotificationViewController)
 class EMNotificationViewController: UIViewController, UNNotificationContentExtension {
 
