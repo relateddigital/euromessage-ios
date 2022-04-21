3416da67ee6e67b1f9d58c31f7f235f746ea9ce5
Umut Can Alparslan <umut_alparslan@hotmail.com>
2022-04-21 15:26:50 +0300

    Silent Push

--- Sources/Euromsg/Classes/EMUtils/EMKey.swift
+++ Sources/Euromsg/Classes/EMUtils/EMKey.swift
@@ -13,7 +13,7 @@ class EMKey {
                     appAlias not provided. Please use Euromsg.configure(::) function first.
                     For more information visit https://github.com/relateddigital/euromessage-ios
                     """
-    internal static let sdkVersion = "2.5.4"
+    internal static let sdkVersion = "2.6.0"
     internal static let tokenKey = "EURO_TOKEN_KEY"
     internal static let registerKey = "EURO_REGISTER_KEY"
     internal static let euroLastMessageKey = "EURO_LAST_MESSAGE_KEY"
