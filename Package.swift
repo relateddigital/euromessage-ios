42933c594f1b46ba76598cb4f6707a8c0ff53613
Umut Can Alparslan <umut_alparslan@hotmail.com>
2025-10-17 16:10:44 +0300

    package update

--- Package.swift
+++ Package.swift
@@ -21,7 +21,13 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Euromsg",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                            // Euromsg SDK içinde Resources klasörü varsa:
+                            .process("Sources")
+                            // ya da farklı isimdeyse .process("EuromsgResources") gibi değiştir
+                        ]
+        ),
         .testTarget(
             name: "EuromsgTests",
             dependencies: ["Euromsg"])
