13de3d08948f160ad22dd5080dfb69ff9eb1a161
Umut Can Alparslan <umut_alparslan@hotmail.com>
2025-10-20 16:48:17 +0300

    package update

--- Package.swift
+++ Package.swift
@@ -1,23 +1,36 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "Euromsg",
-    platforms: [.iOS(.v12)],
+    platforms: [
+        .iOS(.v12)
+    ],
     products: [
+        // 'type: .dynamic' kaldırıldı. Bu, SPM'in hedefe göre en uygun linkleme türünü
+        // (extension'lar için statik) seçmesine olanak tanır.
         .library(
             name: "Euromsg",
             targets: ["Euromsg"]
         )
     ],
-    dependencies: [],
+    dependencies: [
+        // Projenizin başka bağımlılıkları varsa buraya ekleyebilirsiniz.
+    ],
     targets: [
         .target(
             name: "Euromsg",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources/Euromsg/Classes",
+            // Karusel özelliğinin çalışması için gerekli olan .xib dosyalarını
+            // kaynak olarak ekliyoruz.
+            resources: [
+                .process("EMNotificationCarousel/EMNotificationCarousel.xib"),
+                .process("EMNotificationCarousel/CarouselCell.xib")
+            ]
+        ),
         .testTarget(
             name: "EuromsgTests",
             dependencies: ["Euromsg"]
